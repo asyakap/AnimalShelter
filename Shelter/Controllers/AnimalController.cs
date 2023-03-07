2c82af86b9250c380f61b6f88204825c5b71cd32
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-03-07 10:42:09 -0800

    add order by name in Index view

--- Shelter/Controllers/AnimalController.cs
+++ Shelter/Controllers/AnimalController.cs
@@ -16,7 +16,7 @@ namespace Shelter.Controllers
 
     public ActionResult Index()
     {
-      List<Animal> model = _db.Animals.ToList();
+      List<Animal> model = _db.Animals.OrderBy(animal => animal.Name).ToList();
       return View(model);
     }
 
